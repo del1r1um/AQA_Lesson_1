15395007d5cfb181a5cdc0cea956e9bf08fe7d00
Aleksandr Samsonov <therion@luvu.ru>
2021-09-20 19:40:59 +0300

    Added JUnit4

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,32 +1,32 @@
 package ru.netology.service;
 
-import static org.testng.Assert.assertEquals;
+import static org.junit.Assert.assertEquals;
 
 public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void shouldReturn1000for0() {
-        assertEquals(service.remain(0), 1000);
+        assertEquals(1000, service.remain(0));
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void shouldReturn999For1() {
-        assertEquals(service.remain(1), 999);
+        assertEquals(999, service.remain(1));
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void shouldReturn100For900() {
-        assertEquals(service.remain(100), 900);
+        assertEquals(900, service.remain(100));
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void shouldReturn0For1000() {
-        assertEquals(service.remain(1000), 0);
+        assertEquals(0, service.remain(1000));
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void shouldReturn999For1001() {
-        assertEquals(service.remain(1001), 999);
+        assertEquals(999, service.remain(1001));
     }
 }
